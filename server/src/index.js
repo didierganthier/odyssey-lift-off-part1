441d9c5274ef95ad719a9c986bed0d71af30bc6c
Woodens <woodgregory15@gmail.com>
2022-02-04 16:11:22 -0500

    listening to the server

--- server/src/index.js
+++ server/src/index.js
@@ -1,2 +1,12 @@
 const { AppoloServer } = require('apollo-server');
 const typeDefs = require('./schema');
+
+const server = new AppoloServer({typeDefs});
+
+server.listen().then(() => {
+    console.log(`
+    🚀  Server is running!
+    🔉  Listening on port 4000
+    📭  Query at https://studio.apollographql.com/dev
+ `);
+});
